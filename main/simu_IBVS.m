9ce7ba451cc9cb83d1313335dc670573ded5923a
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-06-02 11:12:15 +0200

    organisation esthetique du fichier de simulation simu_IBVS

--- main/simu_IBVS.m
+++ main/simu_IBVS.m
@@ -6,9 +6,14 @@
 close all;
 clear all;
 clc;
-addpath /home/matheus/Documents/MATLAB/turtle_simu/src/*
-
-
+% Add path forf
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/drawing
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/estimation
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/models
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/optimization
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/sampling
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/transformations
+addpath /home/matheus/Documents/MATLAB/turtle_simu/src/visualSeroing
 
 % Rope parameters
 rlen = 0.75; % rope half-length in meters
@@ -18,58 +23,62 @@ hmax = 0.9*rlen; % rope maximum possible sag
 % a catenary with parameters a = h/hmax and b = sin(theta), s = (a,b). h is
 % the current rope sag and theta is the angle between the rope and the
 % robot r2 XZ plane
-s_init = [0.5; 0.5]; % set initial guess for catenary parameters estimation
-lb = [0.01; 0.01]; % params lower bound
-ub = [1; 1]; % params upper bound
+s_init = [0.5; 0.5];    % initial guess for catenary parameters estimation 
+lb     = [0.01; 0.01];  % params lower bound
+ub     = [1; 1];        % params upper bound
 
 % Constants for visual servoing
 a_d = 0.3;
-theta_d = 5; % in degres
+theta_d = 5;    % in degrees
 b_d = sin(deg2rad(theta_d));
 s_d = [a_d; b_d];
 
-% World
-sigma_W = [0; 0; 0; 0; 0; 0]; % World frame (meters and radians)
-% Robot
-turtle_radius = 0.2;
-w_p_r1 = [2.5; 2; 0; 0; 0; 0]; % initial position of turtle r1 in World frame
-w_p_r2 = [1.5; 1; 0; 0; 0; deg2rad(-15)]; % initial position of turtle r2 in World frame
-% Rope
-r1_p_ropeA = [-turtle_radius; 0; 0; 0; 0; 0]; % rope attachment point (pA) on robot r1 in r1-frame
-r2_p_ropeC = [turtle_radius; 0; 0; 0; 0; 0]; % rope attachment point (pC) on robot r2 in r2-frame
-% Camera
-r2_p_cam = [-turtle_radius; 0; 0; 0; 0; 0]; % position of camera embeeded on turtle r2 oin r2-frame
+% World-frame pose in meters and radians
+sigma_W = [0; 0; 0; 0; 0; 0]; 
+% Robots-frames poses wrt World
+w_pose_r1 = [2.5; 2; 0; 0; 0; 0];               % turtle r1 initial pose
+w_pose_r2 = [1.5; 1; 0; 0; 0; deg2rad(-15)];	% turtle r2 initial pose
+turtle_radius = 0.2;                            % turtle radius
+
+% Pose of rope attachment points in robot-frame : pA on r1 and pC on r2
+r1_pose_pA = [-turtle_radius; 0; 0; 0; 0; 0];   % pA expressed in r1-frame
+r2_pose_pC = [turtle_radius; 0; 0; 0; 0; 0];    % pC expressed in r2-frame
+% Camera pose in r2-frame
+r2_pose_cam = [-turtle_radius; 0; 0; 0; 0; 0];
+
+% Set initial velocity of robots
+r1_v_r1 = [0; 0; 0; 0; 0; deg2rad(0)];  % velocity of r1 in robot frame
+r2_v_r2 = [0; 0; 0; 0; 0; deg2rad(0)];  % velocity of r2 in robot frame
+r2_v_r2_h = r2_v_r2;                    % historic of r2_v_r2
+
+% Set image for drawing
+fig = figure('units','normalized','outerposition',[0 0 1 1]);
 
 % Start simualtion
-dt = 0.01; % in seconds
-t_end = 5;
-r1_v_r1 = [0; 0; 0; 0; 0; deg2rad(0)]; % velocity of r1 in robot frame
-r2_v_r2 = [0; 0; 0; 0; 0; deg2rad(0)]; % velocity of r2 in robot frame
-
-fig = figure('units','normalized','outerposition',[0 0 1 1]);
-r2_v_r2_h = r2_v_r2;
+dt = 0.01;  % simulation sampling period in seconds
+t_end = 5;  % simulation duration
 for t = 0:dt:t_end
     % clear figure
     clf(fig);
 
     % Motion equation for robot r1
-    r1_R_w = angles2rotationMatrix(w_p_r1(4:6)); % rotation matrix World-to-robot
-    r1_J_w = angularRateMtx(w_p_r1(4:6)); % World-to-robot
+    r1_R_w = angles2rotationMatrix(w_pose_r1(4:6)); % rotation matrix World-to-robot
+    r1_J_w = angularRateMtx(w_pose_r1(4:6)); % World-to-robot
     w_T_r1 = transf_W2R(r1_R_w',inv(r1_J_w)); % Inverse transformation: R2W
-    w_p_ropeA = w_p_ropeA + w_T_r1*r1_p_ropeA; % position of pA in World-frame
+    w_p_ropeA = w_p_ropeA + w_T_r1*r1_pose_pA; % position of pA in World-frame
     
     % Motion equation for robot r2
-    r2_R_w = angles2rotMtx(w_p_r2(4:6)); % rotation matrix World-to-robot
-    r2_J_w = angularRateMtx(w_p_r2(4:6)); % World-to-robot
+    r2_R_w = angles2rotMtx(w_pose_r2(4:6)); % rotation matrix World-to-robot
+    r2_J_w = angularRateMtx(w_pose_r2(4:6)); % World-to-robot
     w_T_r2 = transf_W2R(r2_R_w',inv(r2_J_w)); % Inverse transformation: R2W
-    w_p_ropeC = w_p_r2 + w_T_r2*r2_p_ropeC; % position of pA in World-frame
-    w_p_cam = w_p_r2 + r2_p_cam; % position of pA in World-frame
+    w_p_ropeC = w_pose_r2 + w_T_r2*r2_pose_pC; % position of pA in World-frame
+    w_p_cam = w_pose_r2 + r2_pose_cam; % position of pA in World-frame
     
     % Retrive rope parameters
-    [a, b] = retrieveCatenaryParams(rlen, hmax, w_p_ropeA, w_p_ropeC, w_p_r2);
+    [a, b] = retrieveCatenaryParams(rlen, hmax, w_p_ropeA, w_p_ropeC, w_pose_r2);
 
     % draw scene
-    drawScene(fig, w_p_r1, w_p_r2, turtle_radius, w_p_ropeA, w_p_ropeC);
+    drawScene(fig, w_pose_r1, w_pose_r2, turtle_radius, w_p_ropeA, w_p_ropeC);
     % draw 2d catenary 
     Pcat2d_samp = draw2dCatenary(fig, w_p_cam, w_p_ropeC, rlen, hmax, [a;b]);
     
@@ -79,8 +88,8 @@ for t = 0:dt:t_end
     r2_v_r2(6) = v_pC(6);
     r2_v_r2_h = [r2_v_r2_h, r2_v_r2];
     % Robots motion...
-    w_p_r1 = w_p_r1 + r1_v_r1*dt; % motion of r1 in World-frame
-    w_p_r2 = w_p_r2 + w_T_r2*r2_v_r2*dt; % motion of r2 in World-frame
+    w_pose_r1 = w_pose_r1 + r1_v_r1*dt; % motion of r1 in World-frame
+    w_pose_r2 = w_pose_r2 + w_T_r2*r2_v_r2*dt; % motion of r2 in World-frame
 
     pause(dt)
 end
