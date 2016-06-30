f9247532064608a042079ace730180321e5b8179
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-06-30 11:00:20 +0200

    add the derivative of new equation of projected catenary. It will yet be validated

--- src/models/catenaryModelDerivative.m
+++ src/models/catenaryModelDerivative.m
@@ -1,27 +1,41 @@
-function value = catenaryModelDerivative(xk,R,Hmax,p,index)
+function value = catenaryModelDerivative(xk,r,hmax,Tcam,s,index)
 % This function calculates the catenary model partials derivatives (dy/da and
 % dy/db);
 % Remember: the residual is defined as rk = yk - y(xk,p), where p is the
 % vector of parameters p = [a b]. If you wish the residuals
 % partial derivatives, please do: drk/dp = -dy(xk,p)/dp;
 
-a = p(1);
-b = p(2);
-C = 2*a*Hmax/(R^2 - (a*Hmax)^2);
-D = (1/C)*acosh(C*a*Hmax + 1);
-switch index
-    case 1 % dy/da, partial with respect to a = h/hmax
-        % drda = d((1/C)*(cosh(C*x./b - C*D)-1)-a*Hmax)/da
-        diCda = -(R^2 + (a*Hmax)^2)/(2*Hmax*a^2); % diCda = d(1/C)/da
-        Ia = diCda*(cosh(C*xk/b - C*D)-1);
-        Ib1 = (C*xk*(R^2 + (a*Hmax)^2))/(a*b*(R^2 - (a*Hmax)^2));
-        Ib2 = -((R^2 + (a*Hmax)^2)*acosh(C*a*Hmax + 1))/(a*(R^2 - (a*Hmax)^2));
-        Ib3 = -(Ib2 + (C*R)^2/(a*sqrt((C*a*Hmax)^2 + 2*C*a*Hmax)));
-        Ib = (1/C)*sinh(C*xk/b - C*D)*(Ib1 + Ib2 + Ib3);
-        I = Ia + Ib;
-        II = -Hmax;
-        value = (I + II);
-    case 2 % dy/b, partial with respect to b = sin(theta)
-        value = -(xk/(b^2))*sinh(C*xk/b - C*D);
+a   = s(1);
+b   = s(2);
+h   = a*hmax;
+C   = 2*a*hmax/(r^2 - (a*hmax)^2);
+D   = (1/C)*acosh(C*a*hmax + 1);
+Tx  = Tcam(1);
+Ty  = Tcam(2);
+Tz  = Tcam(3);
+
+q1 = (b + sqrt(1-b^2)*xk)./(sqrt(1-b^2)*Tx + b*Tz);
+q2 = (Tx - Tz*xk)./(sqrt(1-b^2)*xk + b);
+
+switch index    % dy/da or dy/db
+    case 1      % dy/da, partial with respect to a = h/hmax
+        diCda   = -(r^2 + h^2)/(2*h*a); % diCda = d(1/C)/da
+        AIa     =  diCda*(cosh(C*q2 - C*D)-1);
+        AIb1    =  (C*q2*(r^2 + h^2))/(a*(r^2 - h^2));
+        AIb2    = -((r^2 + h^2)*acosh(C*h + 1))/(a*(r^2 - h^2));
+        AIb3    = -(AIb2 + (C*r)^2/(a*sqrt((C*h)^2 + 2*C*h)));
+        AIb     = -(q1/C)*sinh(C*q2 - C*D)*(AIb1 + AIb2 + AIb3);
+        AI      =  AIa + AIb;
+        AII     = hmax*q1;
+        value   = (AI + AII);
+    case 2      % dy/b, partial with respect to b = sin(theta)
+        dq1db   =  ((b^2 + sqrt(1-b^2))*Tx + (b-b*sqrt(1-b^2)-xk)*Tz)./(sqrt(1-b^2)*(sqrt(1-b^2)*Tx+b*Tz)^2);
+        dq2db   =  -(sqrt(1-b^2) - b*xk)./(sqrt(1-b^2)*(b+sqrt(1-b^2)*xk)^2);
+        BIa     = -(1/C)*dq1db*(cosh(C*q2 - C*D)-1);
+        BIb     = -q1*sinh(C*q2 -C*D)*dq2db;
+        BI      =  BIa + BIb;
+        BII     =  h*dq1db;
+        BIII    =  Ty*dq1db;
+        value   = BI + BII + BIII;
 end
 end
