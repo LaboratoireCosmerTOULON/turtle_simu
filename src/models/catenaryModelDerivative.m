dfc421c53fd8650c42bd4b66432e80bc18336b2c
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-07-05 10:43:37 +0200

    correct error in the calculation of the catenary derivative

--- src/models/catenaryModelDerivative.m
+++ src/models/catenaryModelDerivative.m
@@ -17,22 +17,25 @@ Tz  = Tcam(3);
 q1 = (b + sqrt(1-b^2)*xk)./(sqrt(1-b^2)*Tx + b*Tz);
 q2 = (Tx - Tz*xk)./(sqrt(1-b^2)*xk + b);
 
-switch index    % dy/da or dy/db
-    case 1      % dy/da, partial with respect to a = h/hmax
+% dy/da or dy/db
+switch index
+    % dy/da, partial with respect to a = h/hmax
+    case 1
         diCda   = -(r^2 + h^2)/(2*h*a); % diCda = d(1/C)/da
-        AIa     =  diCda*(cosh(C*q2 - C*D)-1);
-        AIb1    =  (C*q2*(r^2 + h^2))/(a*(r^2 - h^2));
-        AIb2    = -((r^2 + h^2)*acosh(C*h + 1))/(a*(r^2 - h^2));
+        AIa     =  -q1*diCda*(cosh(C*q2 - C*D)-1);
+        AIb1    =  q2*(C*(r^2 + h^2))/(a*(r^2 - h^2));
+        AIb2    = -((r^2 + h^2)/(a*(r^2 - h^2)))*acosh(C*h + 1);
         AIb3    = -(AIb2 + (C*r)^2/(a*sqrt((C*h)^2 + 2*C*h)));
         AIb     = -(q1/C)*sinh(C*q2 - C*D)*(AIb1 + AIb2 + AIb3);
         AI      =  AIa + AIb;
         AII     = hmax*q1;
         value   = (AI + AII);
-    case 2      % dy/b, partial with respect to b = sin(theta)
-        dq1db   =  ((b^2 + sqrt(1-b^2))*Tx + (b-b*sqrt(1-b^2)-xk)*Tz)./(sqrt(1-b^2)*(sqrt(1-b^2)*Tx+b*Tz)^2);
-        dq2db   = -(sqrt(1-b^2) - b*xk)./(sqrt(1-b^2)*(b+sqrt(1-b^2)*xk)^2);
+    % dy/b, partial with respect to b = sin(theta)
+    case 2 
+        dq1db   =  (Tx - xk*Tz)./(sqrt(1-b^2)*(sqrt(1-b^2)*Tx + b*Tz)^2);
+        dq2db   = -((Tx - xk*Tz)*(sqrt(1-b^2) - b*xk))./(sqrt(1-b^2)*(b+sqrt(1-b^2)*xk)^2);
         BIa     = -(1/C)*dq1db*(cosh(C*q2 - C*D)-1);
-        BIb     = -q1*sinh(C*q2 -C*D)*dq2db;
+        BIb     = -q1*dq2db*sinh(C*q2 -C*D);
         BI      =  BIa + BIb;
         BII     =  h*dq1db;
         BIII    =  Ty*dq1db;
