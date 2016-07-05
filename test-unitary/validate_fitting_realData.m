616170c92ad9f4d7a5b2c305164c747ba5edb4b6
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-07-05 10:50:19 +0200

    The fitting with Fmincon and Gauss-Newton was validated in simulation and with real data. Fmincon has had some trouble to converge from s_init=[0.5 0.5], then we used s_init=[0.5 0.6]. Gauss-Newton did not present this problem, however it has a lower performance than fmincon in parameters estimation. Improvements still needed

--- test-unitary/validate_fitting_realData.m
+++ test-unitary/validate_fitting_realData.m
@@ -7,6 +7,7 @@
 close all;
 clear; 
 clc;    
+options = optimoptions('fmincon','Display','off');
 
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/data
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/estimation
@@ -14,16 +15,57 @@ addpath /home/matheus/Documents/MATLAB/turtle_simu/src/models
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/optimization
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/sampling
 
-% Read measurements
+
+%     ANGLE      #IMG
+% -75 degrees : 1 - 35
+% -60 degrees : 36 - 70
+% -45 degrees : 71 - 130
+% -30 degrees : 131 - 180
+% -15 degrees : 181 - 210
+%  15 degrees : 211 - 260
+%  30 degrees : 261 - 310
+%  45 degrees : 311 - 390
+%  60 degrees : 391 - 440
+%  75 degrees : 441 - 482
+
+Svec_gna = zeros(2,482);
+Svec_fmc = zeros(2,482);
+Svec_real = zeros(2,482);
 
 % Read image
-for k=200:10:450;
-    imagename = sprintf('/home/matheus/catkin_ws/imagesturtle/imageturtle_%d.jpeg',k);
+prompt = 'What is the number of the first image to be read?';
+ko = input(prompt);
+
+for k=ko:482;
+    
+    angle = 0;
+    if(k < 36)
+        angle = -75;
+    elseif(k >= 36 && k < 71)
+        angle = -60;
+    elseif(k >= 71 && k < 131)
+        angle = -45;
+    elseif(k >= 131 && k < 181)
+        angle = -30;
+    elseif(k >= 181 && k < 211)
+        angle = -15;
+    elseif(k >= 211 && k < 261)
+        angle = 15;
+    elseif(k >= 261 && k < 311)
+        angle = 30;
+    elseif(k >= 311 && k < 391)
+        angle = 45;
+    elseif(k >= 391 && k < 440)
+        angle = 60;
+    else
+        angle = 75;
+    end
+    
+    imagename = sprintf('/home/matheus/catkin_ws/imagesturtle/imgMap/imageturtle_%d.jpeg',k);
     I = imread(imagename);
-    figure, imshow(I);
     
     % Find white pixels
-    [vi,ui] = find(I == 1); % [row,col] = find()
+    [vi,ui] = find(I); % [row,col] = find()
     ui = ui';
     vi = vi';
     % camera parameters
@@ -34,6 +76,17 @@ for k=200:10:450;
     % transform from pixels to meters
     xi = (ui-uo)./px;
     yi = (vi-vo)./py;
+    % for plotting
+    xp = xi;
+    yp = yi;
+    
+    % If theta is negative, invert xi
+    inv = false;
+    if(mean(xi) > 0)
+        xi = -xi;
+        inv = true;
+    end
+    
     
     % print
     %     for l=1:length(ui)
@@ -45,27 +98,95 @@ for k=200:10:450;
     hmax = 0.40; % cable maximum sag
     
     % Turtlebot features (measurements in SigmaC)
-    Tx      = 0.0;          % distance in x-axis between camera and rope attachment on robot
-    Ty      = -0.11;        % distance in y-axis between camera and rope attachment on robot
-    Tz      = 0.28;         % distance in z-axis between camera and rope attachment on robot
+    Tx      =  -0.005;          % distance in x-axis between camera and rope attachment on robot
+    Ty      = -0.125;        % distance in y-axis between camera and rope attachment on robot
+    Tz      =  0.27;         % distance in z-axis between camera and rope attachment on robot
     Tcam    = [Tx Ty Tz];   % Translation between rope attachment point and camera frame
     
     % Fitting with Gauss-Newton method
-    s_init = [0.5; 0.5]; % set initial guess
+    s_init = [0.5; 0.6]; % set initial guess
     lb = [0.01; 0.01]; % lower bound
     ub = [1; 1]; % upper bound
-    % [p_gna,steps,chisq] = GaussNewton_v2(xi,yi,rlen,hmax,pinit,lb,ub); % minimization with dampled GNA
+    [s_gna,steps,chisq] = GaussNewton_v2(xi,yi,rlen,hmax,s_init,lb,ub,Tcam); % minimization with dampled GNA
     % Fitting with FMINCON
     A = [];
     b = [];
     Aeq = [];
     beq = [];
-    s_fmc = fmincon(@(s_hat)fcout1(xi,yi,rlen,hmax,s_hat,Tcam),s_init,A,b,Aeq,beq,lb,ub); % minimize error on Y axis
-    % drawObjectiveFunction(xi,yi,rlen,hmax,Tcam,1);
+    s_fmc = fmincon(@(s_hat)fcout1(xi,yi,rlen,hmax,s_hat,Tcam),s_init,A,b,Aeq,beq,lb,ub,[],options); % minimize error on Y axis
     
-    fprintf('I_%d : h= %f, theta= %f \n', k, hmax*s_fmc(1), asin(s_fmc)*180/pi);
+    % Calculate catenary from estimated parameters
+    np3d = 1000;
+    Pcat3d_fmc  = catenary3D(rlen,hmax,s_fmc,Tcam,np3d); % estimation from fmincon
+    Pcat2d_fmc = catenaryProjection(rlen,hmax,s_fmc, Pcat3d_fmc(1,:), Pcat3d_fmc(2,:), Pcat3d_fmc(3,:),Tcam); % estimation from fmincon
+    Pcat3d_gna  = catenary3D(rlen,hmax,s_gna,Tcam,np3d); % estimation from fmincon
+    Pcat2d_gna = catenaryProjection(rlen,hmax,s_gna, Pcat3d_gna(1,:), Pcat3d_gna(2,:), Pcat3d_gna(3,:),Tcam); % estimation from fmincon
     
-%     % Plot estimated 2d curves
+    if(inv == true)
+        s_gna(2)        = -s_gna(2);
+        s_fmc(2)        = -s_fmc(2);
+        Pcat2d_gna(1,:) = -Pcat2d_gna(1,:);
+        Pcat2d_fmc(1,:) = -Pcat2d_fmc(1,:);
+    end
+    
+    Svec_real(:,k) = [0.20; angle];
+    Svec_gna(:,k) = [hmax*s_gna(1); asin(s_gna(2))*180/pi];
+    Svec_fmc(:,k) = [hmax*s_fmc(1); asin(s_fmc(2))*180/pi];
+    %     fprintf('I_%d (theta=%d)\nGNA=(%f,%f)\nFMC=(%f,%f)\n', k, angle, hmax*s_gna(1), asin(s_gna(2))*180/pi,hmax*s_fmc(1), asin(s_fmc(2))*180/pi);
+    %     fprintf('I_%d (theta=%d)\nGNA=(%f,%f)\n', k, angle, hmax*s_gna(1), asin(s_gna(2))*180/pi);    
+    
+ 
+    % Plot results
+    clf; % clear figures before plotting
+    % Show image
+    figure(1);
+    imshow(I);
+    % Plot estimation
+    figure(2);
+    plot(xp,yp,'.');
+    hold on
+    plot(Pcat2d_gna(1,:),Pcat2d_gna(2,:));
+    plot(Pcat2d_fmc(1,:),Pcat2d_fmc(2,:));
+    l=legend('Acquired','GNA','FMC');l.Location='best';
+    title('Estimating sag from real data')
+    xlabel('x_{img}(m)')
+    ylabel('y_{img}(m)')
+    set(gca,'Ydir','reverse')
+    fprintf('I_%d (theta=%d)\nGNA=(%f,%f)\nFMC=(%f,%f)\n \n', k, angle, hmax*s_gna(1), asin(s_gna(2))*180/pi,hmax*s_fmc(1), asin(s_fmc(2))*180/pi);
+    
+      % Plot objective fucntion
+%     axismax = 100;
+%     drawObjectiveFunction(xi,yi,rlen,hmax,Tcam,axismax,1);
+
+    % Wait to continue
+    w = waitforbuttonpress;
+    pause(1);
+    
+end
+
+% Plot estimated angle
+figure();
+plot(1:482,Svec_real(1,:))
+hold on
+plot(1:482,Svec_gna(1,:))
+plot(1:482,Svec_fmc(1,:))
+l=legend('real','GNA','FMC');l.Location='best';
+title('Estimating sag from real data')
+xlabel('k')
+ylabel('Rope sag (m)')
+
+% Plot estimated angle
+figure();
+plot(1:482,Svec_real(2,:))
+hold on
+plot(1:482,Svec_gna(2,:))
+plot(1:482,Svec_fmc(2,:))
+l=legend('real','GNA','FMC');l.Location='best';
+title('Estimating angle from real data')
+xlabel('k')
+ylabel('Angle(degrees)')
+    
+    %     % Plot estimated 2d curves
 %     Pcat3d_fmc  = catenary3D(rlen,hmax,s_fmc,Tcam); % estimation from fmincon
 %     Pcat2d_fmc = catenaryProjection(rlen,hmax,s_fmc, Pcat3d_fmc(1,:), Pcat3d_fmc(2,:), Pcat3d_fmc(3,:),Tcam); % estimation from fmincon
 %     % Pcat2d_gna = catenary2D(rlen,hmax,p_gna); % estimation from gna
@@ -98,6 +219,3 @@ for k=200:10:450;
     % ylabel('y')
     % zlabel('z');
     % axis([-1.50 1.50 -1.50 1.50 -hmax 0.5])
-    
-end
-
