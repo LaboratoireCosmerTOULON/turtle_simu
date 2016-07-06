d052c6a1a73f25a33522bca66cd7095b461b0a07
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-07-06 13:46:25 +0200

    rewrite GNA for better comprehension

--- src/optimization/GaussNewton_v2.m
+++ src/optimization/GaussNewton_v2.m
@@ -1,55 +1,78 @@
-function [Pmin,steps,chisq] = GaussNewton_v2(xi,yi,rlen,hmax,pinit,lb,ub,pc)
+function [s_min,steps,chisq] = GaussNewton_v2(xi,yi,rlen,hmax,s_init,lb,ub,pc)
 %
 % GaussNewton_v2 (dampled and bounded version)
 %
 % Gauss-Newton with bound constraints to perform a non-linear least square 
 % Takes as input a row vector of x and y values, and a column vector of
 % initial guesses. Partial derivatives for the jacobian must entered below
-% as a function 'derivative_dydp()'
+% as a function 'derivative_dyds()'
 %
 % rlen : the rope length
 % hmax : the rope maximum sag 
 
 format long
 tol     = 1e-8; 				% set a value for the accuracy
-maxstep = 30; 					% set maximum number of steps to run for
+maxstep = 20; 					% set maximum number of steps to run for
 m       = length(xi); 			% determine number of measurements
-n       = length(pinit); 		% determine number of parameters
-P       = pinit;				% initial guess
-Pold   = P;
+n       = length(s_init); 		% determine number of parameters
+s       = s_init;				% initial guess
+s_old   = s;
 J       = zeros(m,n); 			% create Jacobian matrix
-chisq   = 0; 					% set the merit function chisq to zero
-R       = zeros(m,1); 			% vector of residuals
-DeltaP  = zeros(maxstep,1); 	% vector containing the value p-p_old
-
-
+r       = zeros(m,1); 			% vector of residuals
+delta_s = zeros(maxstep,1); 	% vector containing the value s-s_old
 
 for k = 1:maxstep % iterate through process
-    %disp(Pold);
     
-    chisq = 0; % set merit function to zero for each iteration
-    
+    chisq = 0; % set merit function to zero for each iteration    
     % ----------------------------------------------------------- %
     %        Calculation of residuals and function Jacobian       %
     % ----------------------------------------------------------- %
-       
-    Pcat2d = catenaryProjection(rlen,hmax,P,xi,pc); % calculate the model y = f(xi,p)
+    
+    % Calculate the model y = f(xi,p)
+    Pcat2d = catenaryProjection(rlen,hmax,s,xi,pc);
     y = Pcat2d(2,:);
+    outliers = [];
     for i = 1:m % for all measurements
 %         fprintf('i: %d size(J): %d %d \n',i,size(J));
-        R(i,1) = yi(i) - y(i);
-        chisq = chisq + R(i,1)^2; % calculate the sum of squares of residuals
+        % Calculate the sum of squares of residuals
+        r(i,1)  = yi(i) - y(i);
+        chisq   = chisq + r(i,1)^2;
        
-        % Calculation of the Jacobian (dr/dp)
-        for j=1:n % for all parameters
-            J(i,j) = -catenaryModelDerivative(xi(i),rlen,hmax,pc,P,j); % drdp = -dydp
+        % Calculation of the Jacobian (dr/ds)
+        for j=1:n
+            J(i,j) = -catenaryModelDerivative(xi(i),rlen,hmax,pc,s,j); % drds = -dyds
         end
-        if(J(i,1) < -100.0 || J(i,2) < -100.0 || J(i,1) > 100.0 || J(i,2) > 100.0 || isnan(J(i,1)) || isnan(J(i,2)))
-            J(i,:) = 0;
-%             R(i,:) = [];  
+        
+%         dh = 10^-6;
+%         Fah = catenaryProjection(rlen,hmax,[s(1)+dh;s(2)],xi(i),pc);
+%         Fa  = catenaryProjection(rlen,hmax,s,xi(i),pc);
+%         yah = Fah(2);
+%         ya  = Fa(2);
+%         Fbh = catenaryProjection(rlen,hmax,[s(1);s(2)+dh],xi(i),pc);
+%         Fb  = catenaryProjection(rlen,hmax,s,xi(i),pc);
+%         ybh = Fbh(2);
+%         yb  = Fb(2);
+%         drda   = -(yah - ya)/dh;
+%         drdb   = -(ybh - yb)/dh;
+% %         fprintf('i%d : Ja= %.6f %.6f Jn= %.6f %.6f \n',i,J(i,:),drda,drdb);
+%         J(i,1) = drda;
+%         J(i,2) = drdb;
+        
+        limJ = 50.0;
+        if(J(i,1) < -limJ || J(i,2) < -limJ || J(i,1) > limJ || J(i,2) > limJ || isnan(J(i,1)) || isnan(J(i,2)))
+            outliers(end + 1) = i;
         end
-%         fprintf('J(%d,:)= %.6f %.6f \n',i,J(i,:));
+
     end
+    
+    % Remove outliers
+    J(outliers,:) = [];
+    r(outliers,:) = [];
+    
+%     for i=1:length(r)
+%         fprintf('i%d : J= %.6f %.6f, r= %.6f \n',i,J(i,:),r(i,:));
+%     end
+    
 %     fprintf('mean(J): %.6f, %.6f \n',mean(J));
 %     fprintf('max(J): %.6f, %.6f \n',max(J));
 %     fprintf('max(J): %.6f, %.6f \n',min(J));
@@ -59,29 +82,33 @@ for k = 1:maxstep % iterate through process
     %            Calculate new approximation
     % ----------------------------------------------------------- %
     
-    Lbda = [1.0 1.0]; % initial value for lambda, the dampling coefficient that avoids the parameters estimation outside the bounds 
+    % Initialization of lambda, the dampling coefficient that avoids the parameters estimation outside the bounds 
+    Lbda = [1.0 1.0]; 
     Lambda = diag(Lbda); % built a diagonal matrix of lambdas
-    P = Pold - Lambda*pinv(J)*R;
-    % Ensure that P is not out of bounds
-    while(sum(P < lb) > 0 || sum(P > ub) >0) % while P is out of bounds, reduce lambda by 10
+    s = s_old - Lambda*pinv(J)*r;
+    % Ensure that s is not out of bounds
+    while(sum(s < lb) > 0 || sum(s > ub) >0) % while P is out of bounds, reduce lambda by 10
         for i=1:n
-            if(P(i) < lb(i) || P(i) > ub(i))
+            if(s(i) < lb(i) || s(i) > ub(i))
                 Lbda(i) = 0.1*Lbda(i);
             end 
         end
         Lambda = diag(Lbda);
-        P = Pold - Lambda*pinv(J)*R; 
+        s = s_old - Lambda*pinv(J)*r; 
     end
-%     fprintf('%d: p_old = [%.4f %.4f], Lbda = (%.4f %.4f), deltap = [%.4f %.4f]\n',k,Pold,Lbda,pinv(J)*R);
-    Pmin = P;
+%     fprintf('%d: s_old = [%.4f %.4f], Lbda = (%.4f %.4f), ds = [%.4f %.4f]\n',k,s_old,Lbda,pinv(J)*r);
     
-    
-    DeltaP(k) = (1/sqrt(2))*sqrt((P(1) - Pold(1))^2 + (P(2) - Pold(2))^2); % calculate estimation improvement
-    
-    if (DeltaP(k) <= tol); %if less than tolerance break
+    % For while, the current vector s is the minimizer of objective
+    % function
+    s_min = s;
+    % Calculate estimation improvement and break loop is improvement is
+    % less than tolerance
+    delta_s(k) = (1/sqrt(2))*sqrt((s(1) - s_old(1))^2 + (s(2) - s_old(2))^2);
+    if (delta_s(k) <= tol);
         break
     end
-    Pold = P; %set p to pold
+    % Otherwise continue loop
+    s_old = s; %set s to s_old
 end
 steps = k;
 end
