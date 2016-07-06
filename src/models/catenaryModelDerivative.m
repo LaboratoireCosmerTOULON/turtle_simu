b7dc5b799d449ccf6c677fc587cbb6eaaaf47968
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-07-06 13:43:11 +0200

    rewrite catenaryModelDerivative for better comprehension

--- src/models/catenaryModelDerivative.m
+++ src/models/catenaryModelDerivative.m
@@ -8,8 +8,8 @@ function value = catenaryModelDerivative(xk,r,hmax,Tcam,s,index)
 a   = s(1);
 b   = s(2);
 h   = a*hmax;
-C   = 2*a*hmax/(r^2 - (a*hmax)^2);
-D   = (1/C)*acosh(C*a*hmax + 1);
+C   = 2*h/(r^2 - h^2);
+D   = (1/C)*acosh(C*h + 1);
 Tx  = Tcam(1);
 Ty  = Tcam(2);
 Tz  = Tcam(3);
@@ -21,15 +21,16 @@ q2 = (Tx - Tz*xk)./(sqrt(1-b^2)*xk + b);
 switch index
     % dy/da, partial with respect to a = h/hmax
     case 1
-        diCda   = -(r^2 + h^2)/(2*h*a); % diCda = d(1/C)/da
-        AIa     =  -q1*diCda*(cosh(C*q2 - C*D)-1);
-        AIb1    =  q2*(C*(r^2 + h^2))/(a*(r^2 - h^2));
-        AIb2    = -((r^2 + h^2)/(a*(r^2 - h^2)))*acosh(C*h + 1);
+        d1Cda   = -(r^2 + h^2)/(2*a*h); % d1Cda = d(1/C)/da
+        dCda    =  (C*(r^2 + h^2))/(a*(r^2 - h^2)); 
+        AIa     = -d1Cda*(cosh(C*q2 - C*D)-1);
+        AIb1    =  q2*dCda;
+        AIb2    = -dCda*D;
         AIb3    = -(AIb2 + (C*r)^2/(a*sqrt((C*h)^2 + 2*C*h)));
-        AIb     = -(q1/C)*sinh(C*q2 - C*D)*(AIb1 + AIb2 + AIb3);
+        AIb     = -(1/C)*sinh(C*q2 - C*D)*(AIb1 + AIb2 + AIb3);
         AI      =  AIa + AIb;
-        AII     = hmax*q1;
-        value   = (AI + AII);
+        AII     =  hmax;
+        value   =  q1*(AI + AII);
     % dy/b, partial with respect to b = sin(theta)
     case 2 
         dq1db   =  (Tx - xk*Tz)./(sqrt(1-b^2)*(sqrt(1-b^2)*Tx + b*Tz)^2);
