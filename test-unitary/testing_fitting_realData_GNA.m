22255eb1230d7ef401f1f6ea73b0127d0a25b7cc
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-08-08 17:25:22 +0200

    update script for new dataset

--- test-unitary/testing_fitting_realData_GNA.m
+++ test-unitary/testing_fitting_realData_GNA.m
@@ -7,7 +7,6 @@
 close all;
 clear; 
 clc;    
-options = optimoptions('fmincon','Display','off');
 
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/data
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/drawing
@@ -15,7 +14,6 @@ addpath /home/matheus/Documents/MATLAB/turtle_simu/src/estimation
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/models
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/optimization
 addpath /home/matheus/Documents/MATLAB/turtle_simu/src/sampling
-
 
 %     ANGLE      #IMG
 % -75 degrees : 1 - 35
@@ -29,40 +27,45 @@ addpath /home/matheus/Documents/MATLAB/turtle_simu/src/sampling
 %  60 degrees : 391 - 440
 %  75 degrees : 441 - 482
 
-Svec_gna = zeros(2,482);
-Svec_fmc = zeros(2,482);
-Svec_real = zeros(2,482);
 
 % Read image
 prompt = 'What is the number of the first image to be read?';
 ko = input(prompt);
+prompt = 'Display partial results while running? (1/0)';
+dp = input(prompt);
+nImages = 1237;
 
-for k=ko:482;
+Svec_gna = zeros(2,nImages);
+Svec_real = zeros(2,nImages);
+
+for k=ko:nImages;
     
     angle = 0;
-    if(k < 36)
+    if(k < 120)
         angle = -75;
-    elseif(k >= 36 && k < 71)
+    elseif(k >= 121 && k < 207)
         angle = -60;
-    elseif(k >= 71 && k < 131)
+    elseif(k >= 208 && k < 320)
         angle = -45;
-    elseif(k >= 131 && k < 181)
+    elseif(k >= 321 && k < 522)
         angle = -30;
-    elseif(k >= 181 && k < 211)
+    elseif(k >= 523 && k < 628)
         angle = -15;
-    elseif(k >= 211 && k < 261)
+    elseif(k >= 629 && k < 724)
+        angle = 0;
+    elseif(k >= 725 && k < 822)
         angle = 15;
-    elseif(k >= 261 && k < 311)
+    elseif(k >= 823 && k < 949)
         angle = 30;
-    elseif(k >= 311 && k < 391)
+    elseif(k >= 950 && k < 1059)
         angle = 45;
-    elseif(k >= 391 && k < 440)
+    elseif(k >= 1060 && k < 1179)
         angle = 60;
     else
         angle = 75;
     end
     
-    imagename = sprintf('/home/matheus/catkin_ws/imagesturtle/imgMap/imageturtle_%d.jpeg',k);
+    imagename = sprintf('/home/matheus/catkin_ws/brouillons/imgRope/imgMap/imgMap_%d.jpeg',k);
     I = imread(imagename);
     I = im2bw(I, 0.9);
     
@@ -96,13 +99,13 @@ for k=ko:482;
     %     end
     
     % Rope parameter
-    rlen = 0.48; % cable half-length in meters
-    hmax = 0.40; % cable maximum sag
+    rlen = 0.70; % cable half-length in meters
+    hmax = 0.60; % cable maximum sag
     
     % Turtlebot features (measurements in SigmaC)
     Tx      = -0.01;          % distance in x-axis between camera and rope attachment on robot
-    Ty      = -0.11;        % distance in y-axis between camera and rope attachment on robot
-    Tz      =  0.27;         % distance in z-axis between camera and rope attachment on robot
+    Ty      = -0.10;        % distance in y-axis between camera and rope attachment on robot
+    Tz      =  0.21;         % distance in z-axis between camera and rope attachment on robot
     Tcam    = [Tx Ty Tz];   % Translation between rope attachment point and camera frame
     
     % Fitting with Gauss-Newton method
@@ -127,38 +130,40 @@ for k=ko:482;
     
  
     % Plot results
-    clf; % clear figures before plotting
-    % Show image
-    figure(1);
-    imshow(I);
-    % Plot estimation
-    figure(2);
-    plot(xp,yp,'.');
-    hold on
-    plot(Pcat2d_gna(1,:),Pcat2d_gna(2,:));
-    l=legend('Acquired','GNA');l.Location='best';
-    title('Estimating sag from real data')
-    xlabel('x_{img}(m)')
-    ylabel('y_{img}(m)')
-    set(gca,'Ydir','reverse')
-    axis([-0.64 0.64 -0.48 0.48])
-    fprintf('I_%d (theta=%d)\nGNA=(%f,%f)\n', k, angle, hmax*s_gna(1), asin(s_gna(2))*180/pi);
+    if(dp)
+        clf; % clear figures before plotting
+        % Show image
+        figure(1);
+        imshow(I);
+        % Plot estimation
+        figure(2);
+        plot(xp,yp,'.');
+        hold on
+        plot(Pcat2d_gna(1,:),Pcat2d_gna(2,:));
+        l=legend('Acquired','GNA');l.Location='best';
+        title('Estimating sag from real data')
+        xlabel('x_{img}(m)')
+        ylabel('y_{img}(m)')
+        set(gca,'Ydir','reverse')
+        axis([-0.64 0.64 -0.48 0.48])
+        fprintf('I_%d (theta=%d)\nGNA=(%f,%f)\n', k, angle, hmax*s_gna(1), asin(s_gna(2))*180/pi);
+    end
     
     % Plot objective fucntion
 %     axismax = 10000;
 %     drawObjectiveFunction(xi,yi,rlen,hmax,Tcam,axismax,2);
 
     % Wait to continue
-    w = waitforbuttonpress;
-%     pause(1);
-    
+%     w = waitforbuttonpress;
+    pause(0.1);
+
 end
 
 % Plot estimated angle
 figure();
-plot(1:482,Svec_real(1,:))
+plot(1:nImages,Svec_real(1,:))
 hold on
-plot(1:482,Svec_gna(1,:))
+plot(1:nImages,Svec_gna(1,:))
 l=legend('real','GNA');l.Location='best';
 title('Estimating sag from real data')
 xlabel('k')
@@ -166,9 +171,9 @@ ylabel('Rope sag (m)')
 
 % Plot estimated angle
 figure();
-plot(1:482,Svec_real(2,:))
+plot(1:nImages,Svec_real(2,:))
 hold on
-plot(1:482,Svec_gna(2,:))
+plot(1:nImages,Svec_gna(2,:))
 l=legend('real','GNA');l.Location='best';
 title('Estimating angle from real data')
 xlabel('k')
